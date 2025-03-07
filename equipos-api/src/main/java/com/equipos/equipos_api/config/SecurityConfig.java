29c3d8662ffdd02955953709d0857f586725641a
[marcos montañes] <[marcosmontanesdiad@gmail.com]>
2025-03-07 12:48:53 +0100

    s4

---
+++ equipos-api/src/main/java/com/equipos/equipos_api/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+// package com.equipos.equipos_api.config;
+
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+
+// @Configuration
+// public class SecurityConfig {
+
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         http
+//                 .authorizeHttpRequests(authz -> authz
+//                                 .requestMatchers("/equipos/**").authenticated()
+//                                 .anyRequest().permitAll()
+//                 )
+//                 .formLogin(form -> form
+//                                 .defaultSuccessUrl("/equipos", true) // Redirige a /proyectos tras el login
+//                                 .permitAll()
+//                 )
+//             .logout(logout -> logout.permitAll());
+//         return http.build();
+//     }
+// }
+
